630f74aff1e27f09faa01c0021c272827ed108ec
Vineet kushwaha <kushwahavineet651@gmail.com>
2026-05-02 15:54:20 +0530

    Added: CI

---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1 class="text-4xl">Hello World!</h1>
+    <a href="/about">About page</a>
+    <br/>
+    <NuxtLink to="/products/"> Product list</NuxtLink>
+</template>
+<script leng="ts" setup>
+useHead({
+    title:'Home'
+})
+</script>
---
+++ app/pages/products/[ItemId].vue
@@ -0,0 +1,7 @@
+<template>
+    {{ param }}
+</template>
+<script leng="ts" setup>
+
+const param = useRoute().params.ItemId;
+</script>
