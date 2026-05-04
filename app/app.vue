73562ad983c57e10d893ce80d4dbc8795c7f4995
Vineet kushwaha <kushwahavineet651@gmail.com>
2026-05-05 00:44:35 +0530

    Added: store item in local store

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,14 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useCartStore } from '@/stores/cart'
+
+const cart = useCartStore()
+
+onMounted(() => {
+  cart.loadCart()
+})
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
